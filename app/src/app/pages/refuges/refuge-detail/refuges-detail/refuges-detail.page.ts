331cd34f939d0c5b22416e71c72a9663b8e40a63
Oriol Agost Batalla <gpg18@agost.info>
2023-10-21 23:45:00 +0200

    feat: refuge detail

--- app/src/app/pages/refuges/refuge-detail/refuges-detail/refuges-detail.page.ts
+++ app/src/app/pages/refuges/refuge-detail/refuges-detail/refuges-detail.page.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { Refuge } from '../../../../schemas/refuge/refuge';
+import { ActivatedRoute, Router } from '@angular/router';
+import { RefugeService } from '../../../../services/refuge/refuge.service';
+import { AlertController } from '@ionic/angular';
+import {
+  GetRefugeFromIdErrors,
+  GetRefugeResponse,
+} from '../../../../schemas/refuge/get-refuge-schema';
+import { match } from 'ts-pattern';
 
 @Component({
   selector: 'app-refuges-detail',
@@ -6,7 +15,119 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./refuges-detail.page.scss'],
 })
 export class RefugesDetailPage implements OnInit {
-  constructor() {}
+  refuge?: Refuge;
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private refugeService: RefugeService,
+    private alertController: AlertController,
+  ) {
+    const refugeId = this.getRefugeIdFromUrl();
+    this.fetchRefuge(refugeId).then();
+  }
+
+  getImageUrl(): string | undefined {
+    if (this.refuge == undefined) return undefined;
+    return this.refugeService.getImageUrlFor(this.refuge);
+  }
+
+  clickButton() {
+    console.log('click');
+  }
 
   ngOnInit() {}
+
+  private async fetchRefuge(refugeId: string | null): Promise<void> {
+    if (refugeId != null) this.fetchRefugeFromId(refugeId);
+    else this.router.navigate(['login']).then();
+  }
+
+  private getRefugeIdFromUrl(): string | null {
+    return this.route.snapshot.paramMap.get('id');
+  }
+
+  private fetchRefugeFromId(refugeId: string) {
+    this.refugeService.getRefugeFrom(refugeId).subscribe({
+      next: (response: GetRefugeResponse) =>
+        this.handleGetRefugeResponse(response),
+      error: () => this.handleClientError().then(),
+    });
+  }
+
+  private handleGetRefugeResponse(response: GetRefugeResponse) {
+    match(response)
+      .with({ status: 'correct' }, (response) => (this.refuge = response.data))
+      .with({ status: 'error' }, (response) => {
+        this.handleError(response.error);
+      })
+      .exhaustive();
+  }
+
+  private handleError(error: GetRefugeFromIdErrors) {
+    match(error)
+      .with(GetRefugeFromIdErrors.NOT_FOUND, () => this.handleNotFoundRefuge())
+      .with(GetRefugeFromIdErrors.CLIENT_SEND_DATA_ERROR, () =>
+        this.handleBadUserData(),
+      )
+      .with(GetRefugeFromIdErrors.UNKNOWN_ERROR, () =>
+        this.handleUnknownError(),
+      )
+      .with(
+        GetRefugeFromIdErrors.SERVER_INCORRECT_DATA_FORMAT_ERROR,
+        GetRefugeFromIdErrors.PROGRAMMER_SEND_DATA_ERROR,
+        () => this.handleBadProgrammerData(),
+      )
+      .exhaustive();
+  }
+
+  private async handleClientError() {
+    const alert = await this.alertController.create({
+      header: 'Alert',
+      subHeader: 'The client is failing',
+      message:
+        'Is your internet connection working? Maybe is our fault and our server is down.',
+      buttons: [
+        {
+          text: 'OK',
+          handler: () => {
+            this.alertController.dismiss().then();
+            this.fetchRefuge(this.getRefugeIdFromUrl());
+          },
+        },
+      ],
+    });
+    return await alert.present();
+  }
+
+  private handleNotFoundRefuge() {
+    this.router
+      .navigate(['not-found-page'], {
+        skipLocationChange: true,
+      })
+      .then();
+  }
+
+  private handleBadProgrammerData() {
+    this.router
+      .navigate(['programming-error'], {
+        skipLocationChange: true,
+      })
+      .then();
+  }
+
+  private handleBadUserData() {
+    this.router
+      .navigate(['not-found-page'], {
+        skipLocationChange: true,
+      })
+      .then();
+  }
+
+  private handleUnknownError() {
+    this.router
+      .navigate(['internal-error-page'], {
+        skipLocationChange: true,
+      })
+      .then();
+  }
 }
--- app/src/app/services/refuge/refuge.service.ts
+++ app/src/app/services/refuge/refuge.service.ts
@@ -6,8 +6,12 @@ import {
   GetAllRefugesResponse,
 } from '../../schemas/refuge/get-all-refuges-schema';
 import { environment } from '../../../environments/environment';
-import { Refuge, RefugePattern } from '../../schemas/refuge/refuge';
+import { isValidId, Refuge, RefugePattern } from '../../schemas/refuge/refuge';
 import { isMatching } from 'ts-pattern';
+import {
+  GetRefugeFromIdErrors,
+  GetRefugeResponse,
+} from '../../schemas/refuge/get-refuge-schema';
 
 @Injectable({
   providedIn: 'root',
@@ -43,6 +47,41 @@ export class RefugeService {
 
   private getAllRefugesEndpoint(): string {
     return `${environment.API}/refuges/`;
+  }
+
+  getRefugeFrom(id: string): Observable<GetRefugeResponse> {
+    if (!isValidId(id))
+      return of({
+        status: 'error',
+        error: GetRefugeFromIdErrors.CLIENT_SEND_DATA_ERROR,
+      });
+    return this.getRefugeFromApi(id);
+  }
+
+  private getRefugeFromApi(id: string): Observable<GetRefugeResponse> {
+    const endpoint = this.getRefugeFromIdEndpoint(id);
+    return this.http.get<Refuge>(endpoint).pipe(
+      map<Refuge, GetRefugeResponse | Error>((refuge: Refuge) => {
+        if (isMatching(RefugePattern, refuge))
+          return { status: 'correct', data: refuge };
+        return {
+          status: 'error',
+          error: GetRefugeFromIdErrors.SERVER_INCORRECT_DATA_FORMAT_ERROR,
+        };
+      }),
+      catchError<GetRefugeResponse | Error, ObservableInput<any>>(
+        (err: HttpErrorResponse) =>
+          of({
+            status: 'error',
+            error: GetRefugeFromIdErrors.from(err),
+          }),
+      ),
+      retry(3),
+    );
+  }
+
+  private getRefugeFromIdEndpoint(id: string): string {
+    return `${environment.API}/refuges/${id}/`;
   }
 
   getImageUrlFor(refuge: Refuge): string {
